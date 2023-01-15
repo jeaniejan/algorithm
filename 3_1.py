50382c466d87112d3a53e397f451b5ecbd31efb1
Jin <jeanie0124@naver.com>
2023-01-16 02:57:29 +0900

    1/16 commit

---
+++ 3_1.py
@@ -0,0 +1,16 @@
+#회문 문자열 검사
+N=int(input())
+for i in range(N):
+    s=input()
+    s=s.upper()
+    size=len(s) 
+    
+    for j in range(size//2):
+        if s[j]!=s[-1-j]:
+            print("#%d NO" %(i+1)) 
+            break
+    else:
+        print("#%d YES" %(i+1))
+        
+    
+    
